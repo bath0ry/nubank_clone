f25dbd5d399c862b3b8635072602dad553196cad
Paulo Gomes <plylegacy@gmail.com>
2022-10-01 17:18:19 -0300

    add query

--- lib/home/page/view/home_page.dart
+++ lib/home/page/view/home_page.dart
@@ -11,70 +11,73 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: [
-            Container(
-              color: Color(0xff830ad1),
-              width: 420,
-              height: 150,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.person_outline,
-                            size: 30,
-                            color: Colors.white,
+            MediaQuery(
+              data: MediaQuery.of(context).copyWith(),
+              child: Container(
+                color: Color(0xff830ad1),
+                width: 420,
+                height: 150,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.person_outline,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            alignment: Alignment.topLeft,
                           ),
-                          alignment: Alignment.topLeft,
-                        ),
-                        SizedBox(
-                          width: 185,
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.visibility,
-                              color: Colors.white,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.help_outline,
-                              color: Colors.white,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.person_add_alt,
-                              color: Colors.white,
-                            )),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 10,
+                          SizedBox(
+                            width: 185,
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.help_outline,
+                                color: Colors.white,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.person_add_alt,
+                                color: Colors.white,
+                              )),
+                        ],
                       ),
-                      child: Text(
-                        'Olá, Paulo',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500),
+                      SizedBox(
+                        height: 30,
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                        ),
+                        child: Text(
+                          'Olá, Paulo',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
