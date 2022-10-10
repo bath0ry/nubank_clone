9fa6df5a33376e6f48ae04ac9d7df42418e9f906
Paulo Gomes <plylegacy@gmail.com>
2022-10-10 19:06:00 -0300

    add list view home page

--- lib/home/page/view/home_page.dart
+++ lib/home/page/view/home_page.dart
@@ -12,48 +12,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              HomePageBarWidgets(),
-              SizedBox(
-                height: 25,
-              ),
-              GestureDetector(
-                onTap: (() {}),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 24),
-                      child: Text(
-                        'Conta',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.chevron_right))
-                  ],
-                ),
-              ),
-              Row(
+        body: Column(
+          children: [
+            HomePageBarWidgets(),
+            SizedBox(
+              height: 25,
+            ),
+            GestureDetector(
+              onTap: (() {}),
+              child: Row(
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 24),
                     child: Text(
-                      'R\$ 1,83',
-                      style: TextStyle(fontSize: 24),
+                      'Conta',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
                     ),
                   ),
+                  SizedBox(
+                    width: 250,
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.chevron_right))
                 ],
               ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 24),
+                  child: Text(
+                    'R\$ 1,83',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
                 children: [
                   HomePageListButtons(
                     backgroundColorButton: MaterialStateProperty.all(
@@ -74,10 +75,10 @@ class HomePage extends StatelessWidget {
                         MaterialStateProperty.all(EdgeInsets.all(22)),
                     shapeButton: MaterialStateProperty.all(CircleBorder()),
                     iconButton: Icon(
-                      Icons.payments,
+                      Icons.pix_outlined,
                       color: Colors.black,
                     ),
-                    textButton: 'Pagar',
+                    textButton: 'Área Pix',
                   ),
                   HomePageListButtons(
                     backgroundColorButton: MaterialStateProperty.all(
@@ -86,10 +87,10 @@ class HomePage extends StatelessWidget {
                         MaterialStateProperty.all(EdgeInsets.all(22)),
                     shapeButton: MaterialStateProperty.all(CircleBorder()),
                     iconButton: Icon(
-                      Icons.expand,
+                      Icons.pix_outlined,
                       color: Colors.black,
                     ),
-                    textButton: 'Transferir',
+                    textButton: 'Área Pix',
                   ),
                   HomePageListButtons(
                     backgroundColorButton: MaterialStateProperty.all(
@@ -98,98 +99,120 @@ class HomePage extends StatelessWidget {
                         MaterialStateProperty.all(EdgeInsets.all(22)),
                     shapeButton: MaterialStateProperty.all(CircleBorder()),
                     iconButton: Icon(
-                      Icons.savings,
+                      Icons.pix_outlined,
                       color: Colors.black,
                     ),
-                    textButton: 'Depositar',
+                    textButton: 'Área Pix',
+                  ),
+                  HomePageListButtons(
+                    backgroundColorButton: MaterialStateProperty.all(
+                        Color.fromARGB(255, 222, 221, 221)),
+                    paddingButton:
+                        MaterialStateProperty.all(EdgeInsets.all(22)),
+                    shapeButton: MaterialStateProperty.all(CircleBorder()),
+                    iconButton: Icon(
+                      Icons.pix_outlined,
+                      color: Colors.black,
+                    ),
+                    textButton: 'Área Pix',
+                  ),
+                  HomePageListButtons(
+                    backgroundColorButton: MaterialStateProperty.all(
+                        Color.fromARGB(255, 222, 221, 221)),
+                    paddingButton:
+                        MaterialStateProperty.all(EdgeInsets.all(22)),
+                    shapeButton: MaterialStateProperty.all(CircleBorder()),
+                    iconButton: Icon(
+                      Icons.pix_outlined,
+                      color: Colors.black,
+                    ),
+                    textButton: 'Área Pix',
                   ),
                 ],
               ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                children: [
-                  FittedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(26),
-                      child: Container(
-                        width: 350,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color.fromARGB(255, 219, 218, 218)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 140),
-                          child: TextButton.icon(
-                            style: ButtonStyle(
-                                foregroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                                textStyle: MaterialStateProperty.all(TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.w500))),
-                            label: Text(
-                              'Meus cartões',
-                            ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                FittedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(26),
+                    child: Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromARGB(255, 219, 218, 218)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 140),
+                        child: TextButton.icon(
+                          style: ButtonStyle(
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.black),
+                              textStyle: MaterialStateProperty.all(TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w500))),
+                          label: Text(
+                            'Meus cartões',
+                          ),
+                          onPressed: () {},
+                          icon: IconButton(
                             onPressed: () {},
-                            icon: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.credit_card,
-                                size: 23,
-                              ),
+                            icon: Icon(
+                              Icons.credit_card,
+                              size: 23,
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
+                ),
+              ],
+            ),
+            Divider(
+              height: 50,
+              color: Color.fromARGB(255, 216, 216, 216),
+              thickness: 2,
+              indent: 5,
+              endIndent: 5,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.only(left: 23),
+                child: Row(children: [
+                  Text(
+                    'Cartão de crédito',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.chevron_right,
+                      )),
+                ]),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 23),
+              child: Row(
+                children: [
+                  Text('Fatura atual',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 145, 144, 144))),
                 ],
               ),
-              Divider(
-                height: 50,
-                color: Color.fromARGB(255, 216, 216, 216),
-                thickness: 2,
-                indent: 5,
-                endIndent: 5,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 23),
-                  child: Row(children: [
-                    Text(
-                      'Cartão de crédito',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(
-                      width: 130,
-                    ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.chevron_right,
-                        )),
-                  ]),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 23),
-                child: Row(
-                  children: [
-                    Text('Fatura atual',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromARGB(255, 145, 144, 144))),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
